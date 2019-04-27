2cbb01576ae4f1f4f15e0cd744a76d37f7c0c9d3
catcto <catcto.com@gmail.com>
2019-04-27 17:51:56 +0800

    support twitter

---
+++ test/twitter.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const videoUrlLink = require('..');
+
+describe('videoUrlLink.twitter', () => {
+    const urls = [
+        'https://twitter.com/taylorswift13/status/1121935013484867585',
+        'https://twitter.com/blakelively/status/1045768952872398848'
+    ]
+    urls.forEach(function (url) {
+        it('twitter.getInfo ' + url, (done) => {
+            videoUrlLink.twitter.getInfo(url, { proxy: 'http://myserver:18888' }, (error, info) => {
+                console.log(info);
+                done(error);
+            });
+        });
+    });
+});
